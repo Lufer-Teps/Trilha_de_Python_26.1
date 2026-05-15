69dbc5180c492ef8863fa07a05b919c858ff42f3
Lufer-Teps <biducojunior@gmail.com>
2026-05-15 21:50:53 +0000

    codigo da calculadora de viagem for code.

---
+++ desafio_1.py
@@ -0,0 +1,31 @@
+#coleta de informações do usuário.
+
+
+orcamento= float (input("Boas vindas a calcuadora de gastos, por favor digite o seu orçamento em reais."))
+destino= (input("Qual o destino da viagem ? "))
+passagem=float (input("Qual o custo da passagem ? "))
+diaria=float (input("digite o valor da sua diária em euros."))
+tempo=int (input("quantos dias voce ficara no país de destino ? "))
+
+if orcamento < 0 or passagem < 0 or diaria < 0 or tempo < 0:
+    print("valor invalido, voce digitou uma variavel negativa.")
+else:
+
+#variaveis essenciais.
+    conversao= (diaria * 6.1)
+
+    valor_da_hospedagem= (conversao * tempo)
+
+    custo_total= (passagem + valor_da_hospedagem)
+
+    dinheiro_restante= (orcamento - custo_total)
+    dinheiro_pendente= (custo_total - orcamento)
+
+    #informações finais pro usuario.
+    if custo_total >= orcamento:
+        viagem_invalida=("Seu orçamento é inferior ao custo da viagem, logo ela é inválida. ")
+        print(f"sua viagem não foi possivel pois o seu orçamento de {orcamento} é inferior ao custo da viagem de {custo_total}. Você ainda precisa de {dinheiro_pendente}.")
+    else:
+        viagem_valida=("Sua viagem é válida. Aproveite!")
+        print(f"informações finais: o seu orçamento é de {orcamento} reais. Sua viagem para {destino} durará {tempo}. O custo da sua hospedagem será de {valor_da_hospedagem} o custo total da viagem sera de {custo_total} e ainda sobrará {dinheiro_restante}. ")
+
