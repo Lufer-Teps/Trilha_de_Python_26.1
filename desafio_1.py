66c7e69d5e685de07b099da7b57ca093c00ec4c3
Lufer-Teps <biducojunior@gmail.com>
2026-05-17 14:21:50 -0300

    modificação do código para incluir mensagens de erro e saída caso o usuário insira valores negativos

--- desafio_1.py
+++ desafio_1.py
@@ -1,31 +1,39 @@
 #coleta de informações do usuário.
 
 
-orcamento= float (input("Boas vindas a calcuadora de gastos, por favor digite o seu orçamento em reais."))
+orcamento= float (input("Boas vindas a calcuadora de viagem, por favor digite o seu orçamento em reais."))
+if orcamento <= 0:
+    print("Orçamento inválido, por favor digite um valor positivo.")
+    exit()
 destino= (input("Qual o destino da viagem ? "))
 passagem=float (input("Qual o custo da passagem ? "))
+if passagem <= 0:
+    print("Custo da passagem inválido, por favor digite um valor positivo.")
+    exit()  
 diaria=float (input("digite o valor da sua diária em euros."))
+if diaria <= 0:
+    print("Valor da diária inválido, por favor digite um valor positivo.")
+    exit()
 tempo=int (input("quantos dias voce ficara no país de destino ? "))
-
-if orcamento < 0 or passagem < 0 or diaria < 0 or tempo < 0:
-    print("valor invalido, voce digitou uma variavel negativa.")
-else:
+if tempo <= 0:
+    print("Tempo de estadia inválido, por favor digite um valor positivo.")
+    exit()
 
 #variaveis essenciais.
-    conversao= (diaria * 6.1)
+conversao= (diaria * 6.1)
 
-    valor_da_hospedagem= (conversao * tempo)
+valor_da_hospedagem= (conversao * tempo)
 
-    custo_total= (passagem + valor_da_hospedagem)
+custo_total= (passagem + valor_da_hospedagem)
 
-    dinheiro_restante= (orcamento - custo_total)
-    dinheiro_pendente= (custo_total - orcamento)
+dinheiro_restante= (orcamento - custo_total)
+dinheiro_pendente= (custo_total - orcamento)
 
-    #informações finais pro usuario.
-    if custo_total >= orcamento:
-        viagem_invalida=("Seu orçamento é inferior ao custo da viagem, logo ela é inválida. ")
-        print(f"sua viagem não foi possivel pois o seu orçamento de {orcamento} é inferior ao custo da viagem de {custo_total}. Você ainda precisa de {dinheiro_pendente}.")
-    else:
-        viagem_valida=("Sua viagem é válida. Aproveite!")
-        print(f"informações finais: o seu orçamento é de {orcamento} reais. Sua viagem para {destino} durará {tempo}. O custo da sua hospedagem será de {valor_da_hospedagem} o custo total da viagem sera de {custo_total} e ainda sobrará {dinheiro_restante}. ")
+#informações finais pro usuario.
+if custo_total >= orcamento:
+    viagem_invalida=("Seu orçamento é inferior ao custo da viagem, logo ela é inválida. ")
+    print(f"sua viagem não foi possivel pois o seu orçamento de {orcamento} é inferior ao custo da viagem de {custo_total}. Você ainda precisa de {dinheiro_pendente}.")
+else:
+    viagem_valida=("Sua viagem é válida. Aproveite!")
+    print(f"`{viagem_valida}informações finais: o seu orçamento é de {orcamento} reais. Sua viagem para {destino} durará {tempo}. O custo da sua hospedagem será de {valor_da_hospedagem} o custo total da viagem sera de {custo_total} e ainda sobrará {dinheiro_restante}. ")
 
