a6ae94ccebc1a9175572a71153c7dece8cc5b39b
Lufer-Teps <biducojunior@gmail.com>
2026-05-24 16:58:39 -0300

    formatação da escrita do código de coleta de dados e criação da primeira função: função ataque.

--- desafio_2.py
+++ desafio_2.py
@@ -6,3 +6,15 @@ ataque_1 = int(input("Digite o ataque do primeiro monstro: "))
 monstro_2 = input("Digite o nome do segundo monstro: ")
 vida_2 = int(input("Digite a vida do segundo monstro: "))
 ataque_2 = int(input("Digite o ataque do segundo monstro: "))
+
+resultado_1= ataque_1- vida_2
+resultado_2= ataque_2 -vida_1 
+#2 criação dos parametros pra função de ataque e placar.
+
+def atacar(atacante_1, ataque_do_atacante_1, defensor_2, hp_defensor_2):
+    
+    atacar(monstro_1, ataque_1, monstro_2, resultado_1 )
+    print(f"{atacante_1} ataca {defensor_2} causando {ataque_do_atacante_1} de dano! O defensor está com {hp_defensor_2}")
+
+
+    
